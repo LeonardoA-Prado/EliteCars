a38dd50e88b95302295ee1462f4c39e71c9ba8fa
Leo A. Prado <almeidaprado999@gmail.com>
2025-06-02 20:40:46 +0200

    EliteCars V0.4

---
+++ src/Form/CombustibleType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Combustible;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CombustibleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+             ->add('nombre', TextType::class, [
+                 'label' => 'Tipo de Combustible',
+                 'attr' => [
+                     'class' => 'form-control',
+                     'placeholder' => 'Ingresa el tipo de combustible'
+                 ]
+             ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Combustible::class,
+        ]);
+    }
+}
